931c9e83b2cf7f0c87a6ffd66bb54ef3a0016dbc
Rahul <rahulkarda2002@gmail.com>
2022-05-26 18:46:27 +0530

    CV Screener

---
+++ README.md
@@ -0,0 +1,12 @@
+## CV Screener App 
+Built using HTML, CSS and Javascript to showcase different candidate details. For candidate details, randomuser.me api is used. 
+
+## Tech Stack
+HTML, CSS , JS
+
+## Optimizations
+1. Add filters for finding perfect candidate
+2. Add more details field
+
+## Lessons Learned
+I learned how to integrate a API in a project and how to work with JSON files. I learned about different methods to send requests like fetch, get, and post methods.
---
+++ js/app.js
@@ -0,0 +1,37 @@
+console.log("Welcome to app.js");
+
+
+nextCV();
+// Button listener for next button
+const next = document.getElementById('next');
+next.addEventListener('click', nextCV);
+
+const myLanguages = ['JavaScript','Python','Go','C++','Java','Ruby','Typescript','C#'];
+function nextCV() {
+    fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.results[0].picture.large);
+            let name = (`${data.results[0].name.first} ${data.results[0].name.last}`);
+            let city = (data.results[0].location.country);
+            let avatar = (data.results[0].picture.large);
+            let lang = (myLanguages[Math.floor(Math.random() * 6)]);
+            let email = (data.results[0].email);
+            let image = document.getElementById('image');
+            let profile = document.getElementById('profile');
+            if (data != undefined) {
+                image.innerHTML = `<img src='${avatar}'>`;
+                profile.innerHTML = `<ul class="list-group">
+      <li class="list-group-item">Name : ${name}</li>
+      <li class="list-group-item">Language: ${lang}</li>
+      <li class="list-group-item">Country: ${city}</li>
+      <li class="list-group-item">Email: ${email}</li>
+    </ul>`;
+            }
+            else {
+                alert('End of candidate applications');
+                window.location.reload();
+            }
+        })
+};
